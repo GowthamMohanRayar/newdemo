c1f4e5ffaa49bc215543bf6e85519ead98ccf1e3
ELCOT <ELCOT@Lenovo>
2020-08-23 03:08:50 +0530

    CiCdDemo initial commit

---
+++ src/main/java/com/example/demo2/Bean.java
@@ -0,0 +1,19 @@
+package com.example.demo2;
+
+public class Bean {
+
+	private String name;
+	private String id;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+}
--- src/main/java/com/example/demo2/Controller.java
+++ src/main/java/com/example/demo2/Controller.java
@@ -1,31 +1,41 @@
 package com.example.demo2;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
-	
+
 	@GetMapping("/hello")
 	public String printHello() {
 		return "hello";
 	}
-	
+
 	@GetMapping("/say")
 	public String sayHello() {
 		return "hello";
 	}
-	
+
 	@GetMapping("/says")
 	public String saysHello() {
 		return "saysHello";
 	}
-	
+
 	@GetMapping("/malamalamalemale")
 	public String malamalamalemale() {
 		return "malamalamalemale";
 	}
-	
-	
+
+	@PostMapping("/getById")
+	public Bean getById(@RequestBody(required=true) Bean id) {
+		System.out.println(id.getId());
+		System.out.println(id.getName());
+		Bean bean = new Bean();
+		bean.setId("1");
+		bean.setName("Gowtham");
+		return bean;
+	}
 
 }
